9b3ab61ff4df43f3f3ed2639121a35b8038b9933
Kyle Foster <40616718+Foscat@users.noreply.github.com>
2019-07-01 13:56:17 -0500

    basic spring boot

---
+++ src/test/java/com/wolfpack/ToDoList/ToDoListApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wolfpack.ToDoList;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ToDoListApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
