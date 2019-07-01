2c7af6ecde98c2eb483b162274dc9509c079f386
Kyle <kfizzle_enterprises@yahoo.com>
2019-07-01 14:31:20 -0500

    files and folders

--- src/main/java/com/wolfpack/ToDoList/Controller/MainController.java
+++ src/main/java/com/wolfpack/ToDoList/Controller/MainController.java
@@ -3,6 +3,7 @@ package com.wolfpack.ToDoList.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import com.wolfpack.ToDoList.Repository.TodoRepository;
 
@@ -20,4 +21,6 @@ public class MainController {
 	public String getMakeTodoPage() {
 		return"makeTodo";
 	}
+
+	// @PutMapping("/new")
 }
--- src/main/java/com/wolfpack/ToDoList/Model/TodoList.java
+++ src/main/java/com/wolfpack/ToDoList/Model/TodoList.java
@@ -1,5 +1,23 @@
 package com.wolfpack.ToDoList.Model;
 
+import javax.persistence.Entity;
+
+@Entity
 public class TodoList {
+
+    private Long id;
+    private String title;
+    private String author;
+    private String body;
+
+    public TodoList(){
+
+    }
+
+    public TodoList(String title, String author, String body){
+        this.title = title;
+        this.author = author;
+        this.body = body;
+    }
 
 }
