d07041dabcf7b86b50032c87afa7b7179d60d867
suz <suz@suz-Inspiron-3521>
2019-07-01 15:02:12 -0500

    changes in Main Java

--- src/main/java/com/wolfpack/ToDoList/Model/TodoList.java
+++ src/main/java/com/wolfpack/ToDoList/Model/TodoList.java
@@ -20,4 +20,41 @@ public class TodoList {
         this.body = body;
     }
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	@Override
+	public String toString() {
+		return "TodoList [id=" + id + ", title=" + title + ", author=" + author + ", body=" + body + "]";
+	}
+
 }
