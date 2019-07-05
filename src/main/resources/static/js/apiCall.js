7e97718656fd28d5912a9cb3d92139b842624e1c
Foscat <foscat4o1k@outlook.com>
2019-07-05 10:17:16 -0500

    added external api

--- src/main/resources/static/js/apiCall.js
+++ src/main/resources/static/js/apiCall.js
@@ -1,1 +1,22 @@
 console.log("connected");
+
+apiJoke = () => {
+
+    var queryUrl = "http://api.icndb.com/jokes/random/";
+
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    })
+    .then((res) => {
+        // console.log(res);
+
+        var jokeId = res.value.id;
+        var jokeBody = res.value.joke;
+
+        console.log(jokeBody);
+
+    })
+};
+
+apiJoke();
